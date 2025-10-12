73f2cd4842a0d9dca1dbbabd632ff11cc7c55f6d
seu-username <alexandresilva.02101978@gmail.com>
2025-10-12 02:07:16 -0300

    Alterações nos códigos fontes

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './routes/routes.jsx'
-import GlobalStyle  from './styles/globalStyles.js'
+import GlobalStyle from './styles/globalStyles.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/Projeto-13.streaming-project/">
       <Router />
+      <GlobalStyle />
     </BrowserRouter>
-    <GlobalStyle />
   </React.StrictMode>,
 )
