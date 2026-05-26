c5c993d974c622a247d5555a95d67678490d1136
AlexandreSilva78 <alexandresilva.02101978@gmail.>
2026-05-26 15:36:00 -0300

    Arrumando github

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import Router from './routes/routes.jsx'
 import GlobalStyle from './styles/globalStyles.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Projeto-13.streaming-project/">
+    <HashRouter>
       <Router />
       <GlobalStyle />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Projeto-13.streaming-project/',
+  base: 
+    process.env.NODE_ENV === 'production'
+      ? '/Projeto-13.streaming-project/'
+      : '/',
   plugins: [react()],
 })
 
